b6fd9e485988fbb788a2e6108212d32ffa07f677
tyl020131 <tanyl-pm20@student.tarc.edu.my>
2025-11-25 14:34:10 -0500

    Supabase Fix

--- src/integrations/supabase/client.ts
+++ src/integrations/supabase/client.ts
@@ -13,5 +13,10 @@ export const supabase = createClient<Database>(SUPABASE_URL, SUPABASE_PUBLISHABL
     storage: localStorage,
     persistSession: true,
     autoRefreshToken: true,
-  }
+  },
+  global: {
+    headers: {
+      apikey: SUPABASE_PUBLISHABLE_KEY,
+    },
+  },
 });
